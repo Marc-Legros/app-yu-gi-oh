2c2dd04adda8b95a9302f8962bc6fb6eb9f05675
Marc Legros <auflo2007@gmail.com>
2026-02-26 15:43:27 +0100

    edited store

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 export const store = reactive({
     cards: [], 
+    deck: [],
+    selectedCard: null,
     loading: false,
     error: null,
 })
@@ -12,7 +14,17 @@ export async function fetchCards() {
     try {
         const response = await axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php")
         store.cards = response.data.data
-    }catch{
+    }catch(error){
+        console.error("Erreur récupération cards :", error);
+    }
+}
+
+export async function fetchCardsDetails(id) {
+    store.loading=true
+    try {
+        const response = await axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?id=${id}`)
+        store.selectedCard= response.data.data[0]
+    }catch (error){
         console.error("Erreur récupération cards :", error);
     }
 }
